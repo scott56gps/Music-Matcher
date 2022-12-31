9994e2fbf50812dad268b61b00e449b567a5ed23
Scott Nicholes <scott93gps@gmail.com>
2022-12-31 00:13:23 -0800

    Initial Commit

---
+++ Music Matcher/Music_MatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_MatcherApp.swift
+//  Music Matcher
+//
+//  Created by Scott Nicholes on 12/31/22.
+//
+
+import SwiftUI
+
+@main
+struct Music_MatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
