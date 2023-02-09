bcf4c30cf251182fd58918950177a0979e9cfb5f
Scott Nicholes <scott93gps@gmail.com>
2023-02-08 17:52:32 -0800

    Removed unused AVAudioPlayer code in AudioViewable.

--- Music Matcher/Model/AudioViewable.swift
+++ Music Matcher/Model/AudioViewable.swift
@@ -11,18 +11,5 @@ import AVFoundation
 
 protocol AudioViewable: View {
     associatedtype EnumeratedViewable: RawRepresentable where EnumeratedViewable.RawValue == String
-    var player: AVAudioPlayer? { get }
     var sound: EnumeratedViewable { get }
-}
-
-extension AudioViewable {
-    var player: AVAudioPlayer? {
-        guard let soundURL = Bundle.main.url(forResource: sound.rawValue, withExtension: "mp3") else {
-            return nil
-        }
-        
-        let player = try? AVAudioPlayer(contentsOf: soundURL)
-        player?.prepareToPlay()
-        return player
-    }
 }
