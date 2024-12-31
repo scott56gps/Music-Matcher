5bca4cc25d856182b287d8ed8e7e7c1b3877d494
Scott Nicholes <scott93gps@gmail.com>
2024-12-30 21:48:32 -0800

    Extracted Button View out to separate file

--- Music Matcher/View/TitleScreenView.swift
+++ Music Matcher/View/TitleScreenView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct TitleScreenView: View {
     // EnvironmentObject is, what I believe, an updatable pointer back to a source of truth
     @EnvironmentObject var store: MusicMatcherStore
-    
+
     var body: some View {
         VStack {
             VStack {
@@ -21,19 +21,14 @@ struct TitleScreenView: View {
                 Spacer()
             }
             VStack {
-                Button(action: {
-                    withAnimation {
-                        // We ask the store to update the game state to startGame
-                        // TODO: Create a way to start games of different types
-                        store.dispatch(.startGame(.music))
-                    }
-                }, label: {
-                    Text("Start!")
-                        .font(.title)
-                        .foregroundStyle(Color.black)
-                        .padding(12)
-                        .background(.blue, in: RoundedRectangle(cornerRadius: 12))
-                })
+                CTAButton(
+                    action: {
+                        withAnimation {
+                            // We ask the store to update the game state to startGame
+                            // TODO: Create a way to start games of different types
+                            store.dispatch(.startGame(.music))
+                        }
+                    }, label: Text("Start!"))
                 Spacer()
 
             }
